eb4e5a202ae826d9d43f4d60c5c184eaabddc4cd
amuk1989 <58622904+amuk1989@users.noreply.github.com>
2023-12-05 19:15:18 +0700

    update shader and add config (#6)

--- Assets/Scripts/Utility/Consts.cs
+++ Assets/Scripts/Utility/Consts.cs
@@ -5,5 +5,6 @@ namespace Utility
     public static class Consts
     {
         public static readonly int BallShaderValue = Shader.PropertyToID("_Value");
+        public static readonly int BallColor = Shader.PropertyToID("_BaseColor");
     }
 }
